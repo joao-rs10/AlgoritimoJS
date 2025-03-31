681e2fd8fd2536a29221e4066f3d7efd01eff2b9
João Vitor <joaovitorreis1000@gmail.com>
2025-03-31 09:21:30 -0300

    algoritmo js simples

---
+++ arquivo.js
@@ -0,0 +1,12 @@
+var nome= prompt("Insira seu nome: ");
+alert("Olá " + nome + " seja bem-vindo!");
+
+var num1 = prompt('Insira um número que deseja somar: ');
+var valor = parseInt(num1);
+
+var num2 = prompt('Insira o outro número: ');
+var valor2 = parseInt(num2);
+
+var soma= valor + valor2
+
+alert('O resultado da soma dos números é '+ soma);
